ef9acb6b3e87206571cbb6d825eea41df5eea1e1
Finn Johnsen <johnsenf@gmail.com>
2014-06-02 10:25:17 +0200

    parameterized HQL

--- FakeBank/grails-app/controllers/com/fakebank/AuthController.groovy
+++ FakeBank/grails-app/controllers/com/fakebank/AuthController.groovy
@@ -1,11 +1,8 @@
 package com.fakebank
 
-<<<<<<< HEAD
 import groovy.sql.GroovyRowResult
 import groovy.sql.Sql
 import com.fakebank.*
-=======
->>>>>>> upstream/master
 
 class AuthController {
 	def dataSource
@@ -15,18 +12,11 @@ class AuthController {
 	}
 
 	def authenticate(String username, String password) {
-<<<<<<< HEAD
 		//Login logic
 
                 def row = AccountHolder.find("from AccountHolder as ah where ah.username=? and ah.password=?", [username, password])
 
                 println row
-=======
-
-
-		String hql = "from AccountHolder where username = '$username' and password = '$password'"
-		def row = AccountTransaction.executeQuery(hql)
->>>>>>> upstream/master
 
 		if (row) {
 			flash.message = "Successful login"
